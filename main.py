8011709856a6c0494fa6ab0489404804225ff6e6
Peter Pham <ptrpham2468@gmail.com>
2022-08-30 13:59:36 -0500

    added list backend

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
+#!/usr/bin/env python3
+#############################################################################################################
+# Project Name:         S3_Manager
+# File Name:            main.py
+# Author:               Peter Pham (pxp180041)
+# Date Started:         08/10/2022
+#
+# Description:
+#
+#
+#
+# TODO: implement access key and secret key read in
+# TODO: implement boto3 upload
+# TODO: implement
+#############################################################################################################
+
+################# I M P O R T S #################
+import re
+
 from bs4 import BeautifulSoup
 import requests
-from gui import Ui_MainWindow
-from PyQt6 import QtCore, QtGui, QtWidgets
+# from gui import Ui_MainWindow
+# from PyQt6 import QtCore, QtGui, QtWidgets
 
 
+base_url = "https://www.pokellector.com"
+url_to_scrape = base_url + "/sets"
 
-# function to extract html document from given url
-def getHTMLdocument(url):
-    # request for HTML document of given url
-    response = requests.get(url)
 
-    # response will be provided in JSON format
-    return response.text
-
+#############################################################################################################
+# Function:            main
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/10/2022
+#
+# Description:
+#
+#############################################################################################################
 def main():
     # assign required credentials
     # assign URL
-    base_url = "https://www.pokellector.com"
-    url_to_scrape = base_url + "/sets"
+
 
     # create document
     html_document = getHTMLdocument(url_to_scrape)
@@ -25,29 +46,69 @@ def main():
     # create soap object
     soup = BeautifulSoup(html_document, 'html.parser')
 
-    pokemonCount = 0
+    set_list = set_name_getter(soup)
 
-    with open("output.txt", "w") as file:
+    print(set_list)
+    # TODO: send the list to the UI here
 
-        for button in soup.find_all("a", class_="button"):
-            setPageURL = base_url + button.get("href")
+    print("done")
 
-            print(setPageURL)
-            cardSoup = BeautifulSoup(getHTMLdocument(setPageURL), "html.parser")
-            for pokemon in cardSoup.find_all("div", class_="plaque"):
-                print(pokemon.text)
+#############################################################################################################
+# Function:            main
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/12/2022
+#
+# Description:
+# function to extract html document from given url
+#############################################################################################################
+def getHTMLdocument(url):
+    # request for HTML document of given url
+    response = requests.get(url)
 
-                pokemonCount += 1
-
-        print(pokemonCount)
-
+    # response will be provided in JSON format
+    return response.text
 
 
+#############################################################################################################
+# Function:            main
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/12/2022
+#
+# Description:
+# function to extract html document from given url
+#############################################################################################################
+def set_name_getter(soup):
+
+    pokemonSets = list()
+
+    for button in soup.find_all("a", class_="button"):
+
+        setName = button.get("href").strip()
+        setName = setName[1:-1].replace("-", " ")
+        pokemonSets.append(setName)
+
+
+    return pokemonSets
+
+
+#############################################################################################################
+# Function:            main
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/12/2022
+#
+# Description:
+
+#############################################################################################################
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
     main()
-    MainWindow.show()
-    sys.exit(app.exec())
+    # import sys
+    # app = QtWidgets.QApplication(sys.argv)
+    # MainWindow = QtWidgets.QMainWindow()
+    # ui = Ui_MainWindow()
+    # MainWindow.show()
+    # sys.exit(app.exec())
+
+    # print(setPageURL)
+    # cardSoup = BeautifulSoup(getHTMLdocument(setPageURL), "html.parser")
+    # for pokemon in cardSoup.find_all("div", class_="plaque"):
+    #     print(pokemon.text)
